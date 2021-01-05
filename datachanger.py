2d4c926701d1ca4dd929138d8aa7514725a193fb
Tom Bates <tom@euroabs.com>
2021-01-05 23:11:04 +0000

    fills the output row objects with numbers from csv

--- datachanger.py
+++ datachanger.py
@@ -3,7 +3,9 @@ import pandas
 infilename = "C:\\Git\\source\\repos\\Tom\\CovidDataTransformerPy\\data\\covid_data.csv"
 outfilename = "C:\\Git\\source\\repos\\Tom\\CovidDataTransformerPy\\data\\output.csv"
 
-csvfile = pandas.read_csv(infilename, usecols=['date', 'region', 'number'])
+fieldnames = ['date', 'region', 'number']
+
+csvfile = pandas.read_csv(infilename, usecols=fieldnames)
 csvdates = list(dict.fromkeys(csvfile.date.values))  # use dict to get unique
 csvregions = list(dict.fromkeys(csvfile.region.values))
 
@@ -18,3 +20,16 @@ for date in csvdates:
     outputrowobjects.append(newrowobj)
 
 # loop through csv to fill in outputrowobjects
+for i, inrow in csvfile.iterrows():
+    numberadded = False
+    for outrow in outputrowobjects:
+        if outrow.date == inrow.date:
+            for regionpair in outrow.regionPairs:
+                if regionpair.region == inrow.region:
+                    regionpair.number = inrow.number
+                    numberadded = True
+                    break
+        if numberadded:
+            break
+
+# add header row to output.csv
