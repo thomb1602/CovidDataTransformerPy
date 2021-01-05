6e5affa60a0815a5f17de95886944123f9f0e217
Tom Bates <tom@euroabs.com>
2021-01-05 22:21:40 +0000

    pre-populates the outputrow objects array using dates and regions from csv

--- datachanger.py
+++ datachanger.py
@@ -3,12 +3,16 @@ import pandas
 infilename = "C:\\Git\\source\\repos\\Tom\\CovidDataTransformerPy\\data\\covid_data.csv"
 outfilename = "C:\\Git\\source\\repos\\Tom\\CovidDataTransformerPy\\data\\output.csv"
 
-csvfile = pandas.read_csv(infilename, delimiter=',', header='infer')
-print(csvfile.header)
+csvfile = pandas.read_csv(infilename, usecols=['date', 'region', 'number'])
+csvdates = csvfile.date.values  # need to only get unique
+csvregions = csvfile.region.values
 
-# with open("data\covid_data.csv") as csvfile:
-#     reader = csv.DictReader(csvfile)
+outputrowobjects = []
 
-#     dates = reader['date']
-
-#     print(dates)
+# pre-populate the output row objects with dates and region pairs
+for date in csvdates:
+    newrowobj = type("", (object,), {"date": date, "regionPairs": []})
+    for region in csvregions:
+        regionpair = type("", (object,), {"region": region, "number": "-"})
+        newrowobj.regionPairs.append(regionpair)
+    outputrowobjects.append(newrowobj)
